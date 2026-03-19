19ebc6dced68b863aede32c4bf571f56886ec5e0
dev nihas <gordoporco126@gmail.com>
2026-03-19 13:42:35 -0300

    Add files via upload

---
+++ ErickDouglas0A9.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+import java.util.Scanner;
+
+
+
+public class JavaApplication3 {
+
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] erick = new String[100];
+        erick[0] ="Erick";
+        erick[1] ="Douglas";
+        erick[2] ="Erickzin";
+        erick[3] ="Douglaszin";
+        erick[4] ="Naiara";
+        erick[5] ="Giovanna";
+        erick[6] ="Yasmin";
+        erick[7] ="Duda gorda";
+        erick[8] ="Ray vagabunda";
+        erick[9] ="Roberts";
+        System.out.println(" " + erick[0]);
+        System.out.println(" " + erick[1]);
+        System.out.println(" " + erick[2]);
+        System.out.println(" " + erick[3]);
+        System.out.println(" " + erick[4]);
+        System.out.println(" " + erick[5]);
+        System.out.println(" " + erick[6]);
+        System.out.println(" " + erick[7]);
+        System.out.println(" " + erick[8]);
+        System.out.println(" " + erick[9]);
+        
+    }
+    
+}
